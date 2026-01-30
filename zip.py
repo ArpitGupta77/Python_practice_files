b219e6849072cdfb9b3895bcaf1ae7451552d9f9
Arpit <arpit.22236@gmail.com>
2026-01-30 14:48:03 +0530

    january 1st commit

---
+++ zip.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct 14 00:50:04 2025
+
+@author: arpit
+"""
+
+my_list_1 = [1,2,3,4]
+my_list_2 = ['a','b','c','d']
+
+joined = list(zip(my_list_1, my_list_2))
+print(f'The result of the zip function is {joined} it is of type {type(joined)}')
+
+i,j = zip(*joined)
