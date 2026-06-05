d9ecd05481bf43214014c6e251774a25c232f232
ArpitGupta77 <arpit.22236@gmail.com>
2026-06-05 20:01:31 +0530

    Tuples

---
+++ tuple.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct 14 01:26:12 2025
+
+@author: arpit
+"""
+
+my_tuple = (1,2,3,4)
+my_list = [5,6,7,8]
+my_string = "Australia"
+
+print(my_tuple[0])
+
+print(my_tuple[:3])
+
+my_list[0] = 1000
+
+print(my_list)
+
+my_tuple[0] = 2000
+
+print(my_tuple)
+
+# list and dictonaries are mutable
+# Tuples and Strings are immutable
