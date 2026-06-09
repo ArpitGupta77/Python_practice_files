4ac6f59d1d992cf5791ea3210d312541edf8bae4
ArpitGupta77 <arpit.22236@gmail.com>
2026-06-09 16:40:53 +0530

    function practice

---
+++ files_prac.py
@@ -0,0 +1,97 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jun  5 20:21:41 2026
+
+@author: arpit
+"""
+
+# f = open('kipling.txt','w')
+
+# print(type(f))
+
+# f.write('If you can keep your head while all about you \nare losing theirs\
+# and blaming it on you,\n')
+
+# f.write('If you can trust yourself when all men doubt you,\n\
+# But make allowance for their doubting too;\n')
+
+# f.write('If you can wait and not be tired by waiting,\n\
+# Or being lied about, don\'t deal in lies,\n')
+
+# f.write('Or being hated, don\'t give way to hating,\n\
+# And yet don\'t look too good, nor talk too wise:\n')
+    
+    
+# f.close()
+    
+# f = open('kipling.txt','r')
+
+# print(type(f))
+
+# print(f.read())
+
+# f.close()  
+
+ 
+# f = open('kipling.txt','r')
+# print(f.readline())
+# f.close()
+
+# f = open('kipling.txt','r')
+# print(type(f))
+# print(f.readlines())
+# f.close()    
+    
+    
+    
+# f = open('kipling.txt','r')
+# print(type(f))
+# content=(f.readlines())
+# f.close()    
+    
+    
+    
+# f=open('kipling.txt','a')
+# f.write('If you can dream - and not make your dreams your master;\n\
+# If you can think - and not make your thoughts aim;\n')
+# f.close()
+# f = open ('kipling.txt','r')
+# print(f.read())
+# f.close()
+# print()
+# with open('kipling.txt','r') as f:
+#     for line in f.readlines():
+#         print(line,end='')
+        
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
